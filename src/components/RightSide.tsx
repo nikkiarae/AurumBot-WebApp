23e2fb509f4c68e8de671078c42100ef5cac213d
nikkiarae <nikkiandrewrae@gmail.com>
2025-01-27 13:56:57 +0000

    fix video maybe?

--- src/components/RightSide.tsx
+++ src/components/RightSide.tsx
@@ -1,46 +1,51 @@
-import { Box, Card } from "@mui/material";
+"use client";
+
+import { Box, Card, useTheme } from "@mui/material";
 import { FC } from "react";
 
 const RightSide: FC = () => {
-    return (
-      <Card
-        elevation={2}
+  const theme = useTheme()
+  return (
+    <Card
+      elevation={2}
+      sx={{
+        width: "100%", // Responsive width
+        maxWidth: 390, // Max width to match iPhone 13 Pro
+        aspectRatio: "390 / 774", // Maintains the 9:19 aspect ratio
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        borderRadius: "20px",
+        backgroundColor: "#1c1c1c", // Matches the black theme
+        border: `3px solid ${theme.palette.primary.main}`, // Gold border for style
+        margin: "0 auto", // Centers it horizontally
+      }}
+    >
+      <Box
         sx={{
-          width: "100%", // Responsive width
-          maxWidth: 390, // Max width to match iPhone 13 Pro
-          aspectRatio: "390 / 774", // Maintains the 9:19 aspect ratio
+          width: "100%",
+          height: "100%",
           display: "flex",
           alignItems: "center",
           justifyContent: "center",
-          borderRadius: "20px",
-          backgroundColor: "#1c1c1c", // Matches the black theme
-          border: "3px solid #FFD700", // Gold border for style
-          margin: "0 auto", // Centers it horizontally
         }}
       >
-        <Box
-          sx={{
+        <video
+          autoPlay
+          loop
+          muted 
+          style={{
             width: "100%",
             height: "100%",
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center",
+            objectFit: "contain",
+            pointerEvents: "none",
           }}
         >
-          <video
-            autoPlay 
-            loop
-            style={{
-                width: "100%",
-                height: "100%",
-                objectFit: "contain",
-            }}
-        >
-             <source src="/assets/video.mov" type="video/mp4" />
+          <source src="/assets/video.mov" type="video/mp4" />
         </video>
-        </Box>
-      </Card>
-    );
+      </Box>
+    </Card>
+  );
 };
 
 export default RightSide;
